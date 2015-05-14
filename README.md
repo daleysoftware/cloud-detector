85fddab0b3161eba26108893dd74c395f7167ec0
Matt Pillar <matt@aerofs.com>
2015-05-14 16:49:00 -0700

    Code cleanup and README update.

--- README.md
+++ README.md
@@ -1,24 +1,59 @@
-Cloud Detector
-===
+# Cloud Detector
 
-*Work in progress.*
+Cloud Detector scans your network for traffic from Dropbox, Box, SugarSync, and
+other public cloud storage platforms. Detecting public cloud storage usage is
+often necessary to prevent data leakage in the enterprise.
 
-Cloud Detector scans your network for traffic from Dropbox, Box, SugarSync, and other public cloud storage platforms. Detecting public cloud storage usage is often necessary to prevent data leakage in the enterprise.
+## Limitations
 
-Usage
----
+Currently, Cloud Detector only listens for DNS traffic and is therefore only
+useful for detecting the number of public cloud installations on your network.
+
+In the future, Cloud Detector could be extended to collect information about
+the size of content, frequency of transfer, etc., associated with public cloud
+server usage.
+
+## Installation and Usage
 
 Install the following prerequisites:
 
 * `tshark` (the `wireshark` command line network analyzer).
 * `python2.7`
-* `pyshark` (a pip-installable wrapper for tshark)
+* The following pip modules: `pyshark` and `tabulate`.
 
-Run the Cloud Detector application using the following command:
+Run the Cloud Detector application using the following command. N.B.: you must
+run this application in an appropriate network location, such that it is able
+to see all the relevant traffic in your organization.
 
+```
     python2.7 cloud-detector.py
+```
 
-Limitations
----
+For example,
 
-TODO
+```
+    $ python cloud-detector.py 
+    DNS Query: drive.google.com (src: 172.19.10.242)
+    DNS Query: sugarsync.com (src: 172.19.10.242)
+    DNS Query: sugarsync.com (src: 172.19.10.242)
+    DNS Query: onedrive.google.com (src: 172.19.10.242)
+    DNS Query: onedrive.google.com (src: 172.19.10.242)
+    DNS Query: box.com (src: 172.19.10.242)
+    DNS Query: box.com (src: 172.19.10.242)
+    DNS Query: box.com (src: 172.19.10.242)
+    DNS Query: box.com (src: 172.19.10.242)
+    DNS Query: box.com (src: 172.19.10.242)
+    DNS Query: dropbox.com (src: 172.19.10.242)
+    DNS Query: dropbox.com (src: 172.19.10.242)
+    ^C
+
+    DNS Query Summary
+
+    Domain                 Uniques    Total
+    -------------------  ---------  -------
+    box.com                      1        5
+    drive.google.com             1        1
+    dropbox.com                  1        2
+    onedrive.google.com          1        2
+    sugarsync.com                1        2
+```
--- cloud-detector.py
+++ cloud-detector.py
@@ -1,92 +1,45 @@
 import pyshark
-import re
+import collections
+import tabulate
+import trollius
 
-from trollius.executor import TimeoutError
+public_cloud_domains = [
+    "dropbox.com",
+    "box.com",
+    "sugarsync.com",
+    "icloud.com",
+    "drive.google.com",
+    "onedrive.live.com",
+]
 
-CNAME_DROPBOX = 'DROPBOX'
-CNAME_BOX = 'BOX'
-CNAME_SUGARSYNC = 'SUGARSYNC'
-CNAME_ICLOUD = 'ICLOUD'
-CNAME_GOOGLE = 'GOOGLE DRIVE'
-CNAME_ONE = 'ONE DRIVE'
-
-PUBLIC_CLOUD_DNS_NAMES = dict([
-    (CNAME_DROPBOX, 'dropbox.com'),
-    (CNAME_BOX, 'box.com'),
-    (CNAME_SUGARSYNC, 'sugarsync.com'),
-    (CNAME_ICLOUD, 'icloud.com'),
-    (CNAME_GOOGLE, 'drive.google.com'),
-    (CNAME_ONE, 'onedrive.live.com')
-])
-
-
-def init_dict_count():
-    return {key: 0 for key in PUBLIC_CLOUD_DNS_NAMES}
-
-
-'''
-Setting Filter Parameters
-interface: none
-bpf_filter: port 53 for DNS
-display_filter: retrieve request lookup, more specifically for dropbox.com namespaces
-'''
-
-interfaces = None
-# bpf_filters = port 53'
-display_filters = 'eth.type != 0x86dd and dns.flags.response eq 0 and dns.qry.name matches "dropbox.com" or dns.qry.name matches "box.com" or dns.qry.name matches "sugarsync.com" or dns.qry.name matches "icloud.com" or dns.qry.name matches "drive.google.com" or dns.qry.name matches "onedrive.live.com"'
-
-#
-# filter_string = []
-# for key in PUBLIC_CLOUD_DNS_NAMES:
-# if filter_string:
-# filter_string.append(" || ")
-#     filter_string.append('dns matches "(?i)')
-#     filter_string.append(PUBLIC_CLOUD_DNS_NAMES[key] + '"')
-
-# filter_s = ''.join(filter_string)
-# print filter_s
-
+display_filters = "dns.flags.response eq 0 and " + \
+                  " or ".join(["dns.qry.name matches \"" + d + "\"" for d in public_cloud_domains])
 
 dns_capture = pyshark.LiveCapture(
-    interface=interfaces,
+    interface='any',
     display_filter=display_filters)
 
-uniq_ips = {}  # dictionary look up for unique ips
-
+domain_to_ip_to_count = collections.defaultdict(lambda : collections.defaultdict(int))
 
 def print_callback(packet):
-    print('TIME ACCESSED: %s --- DNS QUERY: %s --- (SRC IP: %s)' % (packet.frame_info.time,
-                                                                    packet.dns.qry_name,
-                                                                    packet.ip.src))
-
-    if packet.ip.src not in uniq_ips:
-        uniq_ips[packet.ip.src] = init_dict_count()
-
-    if re.match(".*dropbox.*", packet.dns.qry_name, re.IGNORECASE):
-        uniq_ips[packet.ip.src][CNAME_DROPBOX] += 1
-    elif re.match(".*box.*", packet.dns.qry_name, re.IGNORECASE):
-        uniq_ips[packet.ip.src][CNAME_BOX] += 1
-    elif re.match(".*sugarsync.*", packet.dns.qry_name, re.IGNORECASE):
-        uniq_ips[packet.ip.src][CNAME_SUGARSYNC] += 1
-    elif re.match(".*icloud.*", packet.dns.qry_name, re.IGNORECASE):
-        uniq_ips[packet.ip.src][CNAME_ICLOUD] += 1
-    elif re.match(".*drive\.google.*", packet.dns.qry_name, re.IGNORECASE):
-        uniq_ips[packet.ip.src][CNAME_GOOGLE] += 1
-    elif re.match(".*onedrive.*", packet.dns.qry_name, re.IGNORECASE):
-        uniq_ips[packet.ip.src][CNAME_ONE] += 1
-
+    src = packet.ip.src
+    domain = packet.dns.qry_name
+    print "DNS Query: %s (src: %s)" % (domain, src)
+    domain_to_ip_to_count[domain][src] += 1
 
 try:
     dns_capture.apply_on_packets(print_callback)
-except (KeyboardInterrupt, SystemExit, TimeoutError):
-    print "Exiting..."
+except (KeyboardInterrupt, SystemExit, trollius.executor.TimeoutError):
+    print
 finally:
-    print "\nSummary Report:"
-    print "{:<20} {:<12} {:<8} {:<14} {:<17} {:<11} {:<14}".format('IP Address', 'Dropbox Hits', 'Box HITS',
-                                                                   'SugarSync Hits', 'Google Drive Hits', 'ICloud Hits',
-                                                                   'One Drive Hits')
-    for k, v in uniq_ips.iteritems():
-        print "{:<20} {:>12} {:>8} {:>14} {:>17} {:>11} {:>14}".format(k, v[CNAME_DROPBOX], v[CNAME_BOX],
-                                                                       v[CNAME_SUGARSYNC], v[CNAME_GOOGLE],
-                                                                       v[CNAME_ICLOUD], v[CNAME_ONE])
+    print
+    print "DNS Query Summary"
+    print
 
+    table = []
+
+    for d in sorted(domain_to_ip_to_count.iterkeys()):
+        ip_to_count = domain_to_ip_to_count[d]
+        table.append([d, len(ip_to_count.keys()), sum(ip_to_count.values())])
+
+    print tabulate.tabulate(table, headers=["Domain", "Uniques", "Total"])
